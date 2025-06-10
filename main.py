ad5f75edef06b421682a31fc39d1f322d87b9a34
cooperbraun13 <cooperjbraun13@gmail.com>
2025-06-09 23:56:15 -0600

    got rid of unnecessary print statements

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import pygame
 from constants import *
 
 def main():  
-  print("Starting Asteroids!")
-  print(f"Screen width: {SCREEN_WIDTH}")
-  print(f"Screen height: {SCREEN_HEIGHT}")
-  
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   
