d438e55696ed7e6d0cbd2e652d551e46560a92f4
cooperbraun13 <cooperjbraun13@gmail.com>
2025-06-12 00:50:46 -0600

    had some nesting issues within circleshape class, but everything is fixed and the collision detection works

--- circleshape.py
+++ circleshape.py
@@ -9,17 +9,17 @@ class CircleShape(pygame.sprite.Sprite):
     else:
       super().__init__()
 
-      self.position = pygame.Vector2(x, y)
-      self.velocity = pygame.Vector2(0, 0)
-      self.radius = radius
+    self.position = pygame.Vector2(x, y)
+    self.velocity = pygame.Vector2(0, 0)
+    self.radius = radius
 
-    def draw(self, screen):
+  def draw(self, screen):
       # sub-classes must override
       pass
 
-    def update(self, dt):
+  def update(self, dt):
       # sub-classes must override
       pass
       
-    def check_collision(self, other):
-      return self.position.distance_to(other.position) <= self.radius + other.radius
+  def check_collision(self, other):
+    return self.position.distance_to(other.position) <= self.radius + other.radius
